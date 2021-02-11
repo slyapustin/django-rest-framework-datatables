8248844d9586f2728e38bb558ae1321cf0b01991
Sergey Lyapustin <s.lyapustin@gmail.com>
2021-02-11 20:00:03 +0200

    Added AlbumGlobalFilter and AlbumFilter

---
+++ example/albums/filters.py
@@ -0,0 +1,24 @@
+from django_filters import rest_framework as filters
+from rest_framework_datatables.django_filters.filters import GlobalFilter
+
+from albums.models import Album
+
+
+class GlobalDateFilter(GlobalFilter, filters.DateFilter):
+    pass
+
+
+class AlbumGlobalFilter(filters.FilterSet):
+    created_on = GlobalDateFilter(field_name="created_on__date", lookup_expr='exact')
+
+    class Meta:
+        model = Album
+        fields = ['created_on']
+
+
+class AlbumFilter(filters.FilterSet):
+    created_on = filters.DateFilter(field_name="created_on__date", lookup_expr='exact')
+
+    class Meta:
+        model = Album
+        fields = ['created_on']
